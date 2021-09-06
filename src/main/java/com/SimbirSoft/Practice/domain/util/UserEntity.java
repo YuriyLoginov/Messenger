7b1f9fdeb895e387928cae67ac3f9c1cd70dbafd
Yuriy Loginov <loginov.yur@gmail.com>
2021-09-06 09:12:42 +0400

    add firstName, lastName

--- src/main/java/com/SimbirSoft/Practice/domain/util/UserEntity.java
+++ src/main/java/com/SimbirSoft/Practice/domain/util/UserEntity.java
@@ -10,11 +10,29 @@ public class UserEntity {
     private Long id;
     private String username;
     private String password;
+    private String firstName;
+    private String lastName;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<MessageEntity> message;
 
     public UserEntity() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public Long getId() {
