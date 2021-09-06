ac29b5e5daea114a1c042cd6ba1ded9c992ecc11
Yuriy Loginov <loginov.yur@gmail.com>
2021-09-06 08:57:57 +0400

    add first Entity

---
+++ src/main/java/com/SimbirSoft/Practice/domain/util/UserEntity.java
@@ -0,0 +1,51 @@
+package com.SimbirSoft.Practice.domain.util;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<MessageEntity> message;
+
+    public UserEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<MessageEntity> getMessage() {
+        return message;
+    }
+
+    public void setMessage(List<MessageEntity> message) {
+        this.message = message;
+    }
+}
