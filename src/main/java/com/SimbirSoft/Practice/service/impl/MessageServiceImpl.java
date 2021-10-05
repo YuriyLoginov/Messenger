50fbacd50c3ad06bb9f4d70f0f9979c3fab245b1
Yuriy Loginov <loginov.yur@gmail.com>
2021-10-05 14:55:26 +0400

    update MessageServiceImpl

--- src/main/java/com/SimbirSoft/Practice/service/impl/MessageServiceImpl.java
+++ src/main/java/com/SimbirSoft/Practice/service/impl/MessageServiceImpl.java
@@ -1,6 +1,7 @@
 package com.SimbirSoft.Practice.service.impl;
 
 import com.SimbirSoft.Practice.dto.message.MessageDto;
+import com.SimbirSoft.Practice.dto.message.MessageMetricsDto;
 import com.SimbirSoft.Practice.exception.NotFoundException;
 import com.SimbirSoft.Practice.model.Message;
 import com.SimbirSoft.Practice.model.Room;
@@ -9,6 +10,7 @@ import com.SimbirSoft.Practice.repository.MessageRepo;
 import com.SimbirSoft.Practice.repository.RoomRepo;
 import com.SimbirSoft.Practice.service.MessageService;
 import lombok.AllArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -18,6 +20,7 @@ import java.util.Optional;
 public class MessageServiceImpl implements MessageService {
     private final RoomRepo roomRepo;
     private final MessageRepo messageRepo;
+    private KafkaTemplate<String, MessageMetricsDto> kafkaTemplate;
 
     @Override
     public void save(MessageDto messageDto, User user) {
@@ -32,5 +35,7 @@ public class MessageServiceImpl implements MessageService {
                 .build();
 
         messageRepo.save(messageToSave);
+        kafkaTemplate.send("metrics_messages",
+                new MessageMetricsDto(messageToSave.getMessage(), user.getUsername()));
     }
 }
